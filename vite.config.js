938259c53306207cc5f4cd9cacc7befea51fcf89
LABCAT <shane.watters.nz@gmail.com>
2025-02-15 12:08:23 +1300

    Refactor to use ES modules for web component

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()], // <-- Add this
+  plugins: [vue()],
   build: {
     lib: {
       entry: './src/main.js',
-      name: 'MyWebComponent',
-      fileName: (format) => `my-web-component.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+      formats: ['es']
+    }
+  }
 });
